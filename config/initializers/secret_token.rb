5fc3fbfb73c4ac9e0497316a19bb760fad21e5a5
Chris Sell <baysideufl@gmail.com>
2014-01-21 22:44:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AguaViva::Application.config.secret_key_base = '7d15e3fa93da991991c62fa151badcbb2227c5a214297fb8ba88b6f57857ed8703d4189399aef5c6ca50da6cc81ff37bf1edc2f05b7ab6c7cf686adbff096790'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AguaViva::Application.config.session_store :cookie_store, key: '_agua_viva_session'
